85c48431413364438b9284d38f8756493200583a
katho <katarino240sx@gmail.com>
2017-09-29 11:20:48 -0500

    tipos de operadores

--- Ej001/Main.java
+++ Ej001/Main.java
@@ -1,4 +1,6 @@
 public class Main
+
+
 //Posibles bibliotecas
 
 {
@@ -11,5 +13,30 @@ public class Main
     {
         //Salida de consola
         System.out.println("Hola mundo!");
+
+       //Instancia
+        Persona persona = new Persona("Juancho", "Lopez", 25, "Masculino");
+
+        //System.out.println(persona.getNombre());
+
+        //&&, ||, >, <, ==, <=, >=, %, *, + , =, /,  !=, !
+
+        int a = 0;
+        int b = 3;
+        Integer c = 0;
+
+        //Imprimir una suma
+        System.out.println(a + b);
+        System.out.println("Hola suma: "+a + b);
+
+
+        //Imprimir una conversión de tipo de dato (casteo (cast))
+        //System.out.println(String.valueOf(a)+String.valueOf(b));
+        //Error, son tipos primitivos
+        //System.out.println(a.toString()+b.toString());
+        //Con wrapper
+        System.out.println(c.toString());
+
+
     }
 }

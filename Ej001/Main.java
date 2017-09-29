e44fff7c98a9fc3560eba456076c38576f1e228b
katho <katarino240sx@gmail.com>
2017-09-29 10:08:43 -0500

    Agregando el primer ejercicio
    Hola mundo!!

---
+++ Ej001/Main.java
@@ -0,0 +1,14 @@
+public class Main
+//Posibles bibliotecas
+
+{
+    //Declaración de variables
+
+
+
+    //Mi método main
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo!");
+    }
+}
